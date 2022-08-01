d6c68626ded1f0d5df1909771d056b38c56145e4
Kyle Glaws <kyle.glaws@gmail.com>
2022-07-31 21:53:30 -0400

    index.js now reads test files

--- index.js
+++ index.js
@@ -6,16 +6,32 @@ const fs = require('fs');
 module.exports = Chess;
 
 
-const main = async function() {
+const usage = () => console.log(`${process.argv[0]} ${process.argv[1]} <path/to/input/file>`);
+
+
+const main = (moves) => {
     const game = new Chess();
 
-    while (game.gameOver() === false) {
+    while (game.gameOver() === false && game.turn < moves.length) {
+        const [piece, address] = moves[game.turn];
+        game.doMove(piece, address);
     }
 
     console.log('GAME OVER');
 }
 
-
 if (require.main === module) {
-    main();
+    if (process.argv.length !== 3) {
+        usage();
+        process.exit(1);
+    }
+
+    const moves = fs.readFileSync(process.argv[2])
+        .toString()
+        .replaceAll('\r\n', '\n')  // in case you have windows' line endings
+        .split('\n')
+        .filter(line => line.includes(' '))
+        .map(line => line.split(' '));
+
+    main(moves);
 }
