40ecbde8c51c7a22682f0482625015d780f5fc5b
Kyle Glaws <kyle.glaws@gmail.com>
2022-11-10 22:58:57 -0500

    feature/chess-notation - remove some old tests and add new castling tests in board.test.js

--- tests/board.test.js
+++ tests/board.test.js
@@ -99,12 +99,19 @@ test('correct piece placement on board', () => {
 });
 
 
-test('board build move castle', () => {
+test('pathClear works', () => {
     let board = new Board();
+
+    expect(board.pathClear(5, 5, 2, 2)).toBe(true);
+    expect(board.pathClear(5, 5, 1, 1)).toBe(true);
+    expect(board.pathClear(6, 6, 2, 2)).toBe(true);
+    expect(board.pathClear(6, 6, 1, 1)).toBe(true);
+    expect(board.pathClear(6, 6, 0, 0)).toBe(false);
+    expect(board.pathClear(7, 7, 1, 1)).toBe(false);
 });
 
 
-test('checkForCheck function works', () => {
+test('check detection', () => {
     let board = new Board();
     let blackQueen = new Queen(Color.BLACK, 0, 1); 
     let whiteKing = new King(Color.WHITE, 0, 0);
@@ -128,7 +135,7 @@ test('checkForCheck function works', () => {
 });
 
 
-test('check mate detection works', () => {
+test('checkmate detection', () => {
     let board = new Board();
 
     expect(board.checkForCheck(Color.WHITE)).toBe(false);
@@ -153,7 +160,7 @@ test('check mate detection works', () => {
 });
 
 
-test('stalemate detection works', () => {
+test('stalemate detection', () => {
     let board = new Board();
 
     expect(board.canMove(Color.WHITE)).toBe(true);
@@ -177,193 +184,138 @@ test('stalemate detection works', () => {
 });
 
 
-test('board moves pieces, and move history behaves correctly', () => {
-    let board = new Board();
-    let pawnCBlack = board.get(2, 1);
-
-    // before move
-    expect(board.get(2, 3)).toBe(null);
-    expect(pawnCBlack.x).toBe(2);
-    expect(pawnCBlack.y).toBe(1);
-    expect(board.moveHistory.length).toBe(0);
-
-    // do move
-    board.move(pawnCBlack, 2, 3);
-
-    // after move
-    expect(board.get(2, 1)).toBe(null);
-    expect(board.get(2, 3)).toBe(pawnCBlack);
-    expect(pawnCBlack.x).toBe(2);
-    expect(pawnCBlack.y).toBe(3);
-    expect(board.moveHistory.length).toBe(1);
-    expect(board.moveHistory[0].pieceMoved).toBe(pawnCBlack);
-    expect(board.moveHistory[0].originX).toBe(2);
-    expect(board.moveHistory[0].originY).toBe(1);
-    expect(board.moveHistory[0].pieceTaken).toBe(null);
-
-    // undo
-    board.undo();
-
-    // after undo
-    expect(board.get(2, 1)).toBe(pawnCBlack);
-    expect(board.get(2, 3)).toBe(null);
-    expect(pawnCBlack.x).toBe(2);
-    expect(pawnCBlack.y).toBe(1);
-    expect(board.moveHistory.length).toBe(0);
-
-  
-});
-
-
-test('pathClear works', () => {
+test('castling', () => {
     let board = new Board();
 
-    expect(board.pathClear(5, 5, 2, 2)).toBe(true);
-    expect(board.pathClear(5, 5, 1, 1)).toBe(true);
-    expect(board.pathClear(6, 6, 2, 2)).toBe(true);
-    expect(board.pathClear(6, 6, 1, 1)).toBe(true);
-    expect(board.pathClear(6, 6, 0, 0)).toBe(false);
-    expect(board.pathClear(7, 7, 1, 1)).toBe(false);
-});
+    // castling shouldn't work at game start
+    expect(board.buildMove('O-O-O', Color.BLACK)).toBe(null);
+    expect(board.buildMove('O-O', Color.BLACK)).toBe(null);
+    expect(board.buildMove('O-O-O', Color.WHITE)).toBe(null);
+    expect(board.buildMove('O-O', Color.WHITE)).toBe(null);
 
-// test('promote works', () => {
-//     let board = new Board();
-//     let pawn = new Pawn(Color.WHITE, 0, 0);
-
-//     expect(board.promote(pawn, 'quan')).toBe(false);
-//     expect(board.promote(pawn, 'king')).toBe(false);
-
-//     expect(board.promote(pawn, 'queen')).toBe(true);
-//     expect(board.promote(pawn, 'rook')).toBe(true);
-//     expect(board.promote(pawn, 'bishop')).toBe(true);
-//     expect(board.promote(pawn, 'knight')).toBe(true);
-// });
-
-// try to test castling basic features, 
-// skip castling out of check, castling function is not called in chess.move if check = true, 
-// so castling does not check if king is currently in check, and only checks if the spaces for castling are
-test('castling works', () => {
-    let board = new Board();
-
+    // wipe board
     board.wipe();
 
+    let blackKing = new King(Color.BLACK, 4, 0);
+    let blackRook = new Rook(Color.BLACK, 0, 0);
     let whiteKing = new King(Color.WHITE, 4, 7);
-    board.set(4, 7, whiteKing);
+    let whiteRook = new Rook(Color.WHITE, 7, 7);
 
-    board.teamMap[Color.WHITE] = [whiteKing]
+    // add black king + rooks
+    board.blackKing = blackKing;
+    board.teamMap[Color.BLACK] = [
+        board.set(0, 0, blackRook),
+        board.set(4, 0, blackKing)
+    ];
+
+    // add white king + rooks
     board.whiteKing = whiteKing;
+    board.teamMap[Color.WHITE] = [
+        board.set(7, 7, whiteRook),
+        board.set(4, 7, whiteKing)
+    ];
 
-    // if rook has been captured, castling not possible 
-    let whiteRook = null;
-    expect(board.castle('O-O-O', Color.WHITE)).toBe(null);
+    // black queen side castle
+    let moveList = board.buildMove("O-O-O", Color.BLACK);
+    expect(moveList !== null).toBe(true);
+    expect(moveList.length).toBe(2);
+    expect(moveList[0].piece).toBe(blackKing);
+    expect(moveList[0].destX).toBe(2);
+    expect(moveList[0].destY).toBe(0);
+    expect(moveList[0].firstMove).toBe(false);
+    expect(moveList[1].piece).toBe(blackRook);
+    expect(moveList[1].destX).toBe(3);
+    expect(moveList[1].destY).toBe(0);
+    expect(moveList[1].firstMove).toBe(false);
 
-    /**------------------------------------------------------ */
-    // first move needs to be true in order to castle 
+    // execute the move
+    board.move(moveList);
+    expect(blackKing.x).toBe(2);
+    expect(blackKing.y).toBe(0);
+    expect(blackKing.firstMove).toBe(false);
+    expect(blackRook.x).toBe(3);
+    expect(blackRook.y).toBe(0);
+    expect(blackRook.firstMove).toBe(false);
+    expect(board.get(0, 0)).toBe(null);
+    expect(board.get(2, 0)).toBe(blackKing);
+    expect(board.get(3, 0)).toBe(blackRook);
+    expect(board.get(4, 0)).toBe(null);
 
-    whiteRook = new Rook(Color.WHITE, 7, 7);
-    board.teamMap[Color.WHITE].push(whiteRook);
-    board.set(7, 7, whiteRook);
+    // check prevMove
+    expect(board.prevMove[0].piece).toBe(blackRook);
+    expect(board.prevMove[0].destX).toBe(0);
+    expect(board.prevMove[0].destY).toBe(0);
+    expect(board.prevMove[0].firstMove).toBe(true);
+    expect(board.prevMove[1].piece).toBe(blackKing);
+    expect(board.prevMove[1].destX).toBe(4);
+    expect(board.prevMove[1].destY).toBe(0);
+    expect(board.prevMove[1].firstMove).toBe(true);
 
-    expect(whiteKing.firstMove).toBe(true);
-    expect(whiteRook.firstMove).toBe(true);
-
-    // this is failing because a successful castle returns a new move obj
-    // maybe its best to just test castling through buildMove function?
-
-    expect(board.castle('O-O', Color.WHITE)).toBe(/** should be a move obj */);
-
-    // first move of rook and king is false after castle 
-    expect(whiteKing.firstMove).toBe(false);
-    expect(whiteRook.firstMove).toBe(false);
-
-    /**------------------------------------------------------ */
-    // cannot castle if pieces are in between (done)
-
-    board.board = Array(8).fill().map(() => Array(8).fill(null));
-
-    whiteKing = new King('king', Color.WHITE, 4, 7);
-    board.set(4, 7, whiteKing);
-
-    whiteRook = new Rook('rookK', Color.WHITE, 7, 7);
-    board.set(7, 7, whiteRook);
-
-    let whiteBishop = new Bishop('bishopK', Color.WHITE, 5, 7);
-    board.set(5, 7, whiteBishop);
-
-    expect(board.castle('k', Color.WHITE)).toBe(false);
-
-    // move bishop 
-    board.set(6, 6, whiteBishop);
-    board.set(5, 7, null);
-
-    expect(board.castle('k', Color.WHITE)).toBe(true);
-
-    /**------------------------------------------------------ */
-    // castling queen side
-
-    let blackKing = new King('king', Color.BLACK, 4, 0);
-    board.set(4, 0, blackKing);
-
-    let blackRook = new Rook('rookQ', Color.BLACK, 0, 0);
-    board.set(0, 0, blackRook);
-
-    expect(board.castle('q', Color.BLACK)).toBe(true);
-
-    /**------------------------------------------------------ */
-    // cannot castle across the line of check
-
-    board.board = Array(8).fill().map(() => Array(8).fill(null));
-
-    blackKing = new King('king', Color.BLACK, 4, 0);
-    board.set(4, 0, blackKing);
-
-    blackRook = new Rook('rookQ', Color.BLACK, 0, 0);
-    board.set(0, 0, blackRook);
-
-    let whiteQueen = new Queen('queen', Color.WHITE, 3, 7);
-    board.set(3, 7, whiteQueen);
-
-    expect(board.castle('q', Color.BLACK)).toBe(false);
-
-    // move queen
-    board.set(0, 7, whiteQueen);
-    board.set(3, 7, null);
-    whiteQueen.x = 0;
-
-    expect(board.castle('q', Color.BLACK)).toBe(true);
-});
-
-test('en passant move history', () => {
-    let board = new Board();
-    board.board = Array(8).fill().map(() => Array(8).fill(null));
-
-    let kingWhite = new King('king', Color.WHITE, 4, 7);
-    board.set(4, 7, kingWhite);
-    let pawnDWhite = new Pawn('pawnD', Color.WHITE, 3, 4);
-    pawnDWhite.enPassantable = true;
-    board.set(3, 4, pawnDWhite);
-
-    let kingBlack = new King('king', Color.BLACK, 4, 0);
-    board.set(4, 0, kingBlack);
-    let pawnEBlack = new Pawn('pawnE', Color.BLACK, 4, 4);
-    board.set(4, 4, pawnEBlack);
-
-    expect(board.moveHistory.length).toBe(0);
-    board.move(pawnEBlack, 3, 5);
-    expect(board.moveHistory.length).toBe(1);
-    expect(board.moveHistory[0].pieceMoved).toBe(pawnEBlack);
-    expect(board.moveHistory[0].pieceTaken).toBe(pawnDWhite);
-    expect(board.moveHistory[0].originX).toBe(4);
-    expect(board.moveHistory[0].originY).toBe(4);
-    expect(board.moveHistory[0].destinationX).toBe(3);
-    expect(board.moveHistory[0].destinationY).toBe(5);
-
+    // undo the move
     board.undo();
-    expect(board.moveHistory.length).toBe(0);
-    expect(board.get(3, 5)).toBe(null);
-    expect(board.get(4, 4)).toBe(pawnEBlack);
-    expect(board.get(3, 4)).toBe(pawnDWhite);
+    expect(board.prevMove).toBe(null);
+    expect(board.get(0, 0)).toBe(blackRook);
+    expect(blackRook.firstMove).toBe(true);
+    expect(blackRook.x).toBe(0);
+    expect(blackRook.y).toBe(0);
+    expect(board.get(2, 0)).toBe(null);
+    expect(board.get(3, 0)).toBe(null);
+    expect(board.get(4, 0)).toBe(blackKing);
+    expect(blackKing.firstMove).toBe(true);
+    expect(blackKing.x).toBe(4);
+    expect(blackKing.y).toBe(0);
+
+    // white king side castle
+    moveList = board.buildMove("O-O", Color.WHITE);
+    expect(moveList !== null).toBe(true);
+    expect(moveList.length).toBe(2);
+    expect(moveList[0].piece).toBe(whiteKing);
+    expect(moveList[0].destX).toBe(6);
+    expect(moveList[0].destY).toBe(7);
+    expect(moveList[0].firstMove).toBe(false);
+    expect(moveList[1].piece).toBe(whiteRook);
+    expect(moveList[1].destX).toBe(5);
+    expect(moveList[1].destY).toBe(7);
+    expect(moveList[1].firstMove).toBe(false);
+
+    // execute the move
+    board.move(moveList);
+    expect(whiteKing.x).toBe(6);
+    expect(whiteKing.y).toBe(7);
+    expect(whiteKing.firstMove).toBe(false);
+    expect(whiteRook.x).toBe(5);
+    expect(whiteRook.y).toBe(7);
+    expect(whiteRook.firstMove).toBe(false);
+    expect(board.get(7, 7)).toBe(null);
+    expect(board.get(6, 7)).toBe(whiteKing);
+    expect(board.get(5, 7)).toBe(whiteRook);
+    expect(board.get(4, 7)).toBe(null);
+
+    // check prevMove
+    expect(board.prevMove[0].piece).toBe(whiteRook);
+    expect(board.prevMove[0].destX).toBe(7);
+    expect(board.prevMove[0].destY).toBe(7);
+    expect(board.prevMove[0].firstMove).toBe(true);
+    expect(board.prevMove[1].piece).toBe(whiteKing);
+    expect(board.prevMove[1].destX).toBe(4);
+    expect(board.prevMove[1].destY).toBe(7);
+    expect(board.prevMove[1].firstMove).toBe(true);
+
+    // undo the move
+    board.undo();
+    expect(board.prevMove).toBe(null);
+    expect(board.get(7, 7)).toBe(whiteRook);
+    expect(whiteRook.firstMove).toBe(true);
+    expect(whiteRook.x).toBe(7);
+    expect(whiteRook.y).toBe(7);
+    expect(board.get(6, 7)).toBe(null);
+    expect(board.get(5, 7)).toBe(null);
+    expect(board.get(4, 7)).toBe(whiteKing);
+    expect(whiteKing.firstMove).toBe(true);
+    expect(whiteKing.x).toBe(4);
+    expect(whiteKing.y).toBe(7);
 });
+
 
 test('toString works', () => {
     let board = new Board();
