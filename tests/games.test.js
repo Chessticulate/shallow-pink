460305b6bcbb3ed9d16c4c97796588770e168893
Bglaws <glaws.brian@gmail.com>
2022-11-26 23:28:58 -0500

    progress on chess.test, added games.test

---
+++ tests/games.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Chess = require('../lib/chess');
+const Board = require('../lib/board');
+const Status = require('../lib/status');
+const King = require('../lib/pieces/king');
+const Color = require('../lib/color');
+const Queen = require('../lib/pieces/queen');
+const Rook = require('../lib/pieces/rook');
+const Pawn = require('../lib/pieces/pawn');
+
+test('full game (checkmate)', () => {
+
+});
+
+test('full game (stalemate)', () => {
+
+});
+
+test('draw (insufficient material)', () => {
+
+});
+
+test('draw (50 move rule)', () => {
+
+});
+
+test('draw (threefold repetition)', () => {
+
+});
