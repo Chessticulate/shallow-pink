361baf893814198616c77c4d93754938aabd4051
Bglaws <glaws.brian@gmail.com>
2023-02-13 22:16:02 -0500

    finished ai.test generateMoveStr

--- tests/ai.test.js
+++ tests/ai.test.js
@@ -1,6 +1,7 @@
 const Color = require('../lib/color');
 const Board = require('../lib/board');
 const Pawn = require('../lib/pieces/pawn');
+const King = require('../lib/pieces/king');
 
 const AI = require('../lib/ai');
 
@@ -14,22 +15,37 @@ test('generateMoveStr', () => {
     let knightQWhite = board.get(1, 7);
 
     expect(AI.generateMoveStr(knightQWhite, board, 2, 5)).toBe('Nc3');
+    
+    let i = 1;
 
     ["d4", "Nc6", "d5", "e5"].forEach(move => {
-        board.move(board.buildMove(move));
+        board.move(board.buildMove(move, (i % 2 ? Color.WHITE : Color.BLACK)));
+        i++;
     });
 
     let pawnDWhite = board.get(3, 3);
     expect(AI.generateMoveStr(pawnDWhite, board, 4, 2)).toBe("dxe6");
 
     board.wipe();
-    board.whiteKing = board.set(new King(Color.WHITE, 7, 7));
-    board.blackKing = board.set(new King(Color.BLACK, 7, 1));
-    let whitePawn = board.set(new Pawn(Color.WHITE, 1, 1));
-    board.teamMap[Color.WHITE] = [whiteKing, whitePawn];
-    board.teamMap[Color.BLACK] = [blackKing];
+    let whiteKing = new King(Color.WHITE, 7, 7);
+    let blackKing = new King(Color.WHITE, 1, 7);
+    let whitePawn = new Pawn(Color.WHITE, 1, 1);
+    board.whiteKing = whiteKing;
+    board.blackKing = blackKing;
+
+    board.teamMap[Color.WHITE] = [
+        board.set(0, 7, whiteKing),
+        board.set(3, 1, whitePawn)
+    ];
+    board.teamMap[Color.BLACK] = [
+        board.set(7, 7, blackKing)
+    ];
 
     expect(AI.generateMoveStr(whitePawn, board, 1, 0)).toBe("b8=Q");
+});
+
+test('validMoves', () => {
+    
 });
 
 
