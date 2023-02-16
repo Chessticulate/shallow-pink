7c171cb002d20ad9e11b073b3bbb17f680b382fb
Bglaws <glaws.brian@gmail.com>
2023-02-16 18:02:01 -0500

    progress on ai.test validMoves

--- tests/ai.test.js
+++ tests/ai.test.js
@@ -45,21 +45,54 @@ test('generateMoveStr', () => {
     expect(AI.generateMoveStr(whitePawn, board, 1, 0)).toBe("b8=Q");
 });
 
-test.only('validMoves', () => {
-    // Starting position fen str
-    let fenStr = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    let moveSet = AI.validMoves(fenStr);
+test('validMoves', () => {
+    let chess = new Chess();
+    let moveSet = AI.validMoves(chess.toFEN());
 
     // all possible moves from opening position
-    let openingMoveSet = [
+    let whiteOpeningMoveSet = [
         'a3',  'a4',  'b3',  'b4',
         'c3',  'c4',  'd3',  'd4',
         'e3',  'e4',  'f3',  'f4',
         'g3',  'g4',  'h3',  'h4',
         'Nc3', 'Na3', 'Nh3', 'Nf3'
     ];
+    let blackOpeningMoveSet = [
+        'Nc6', 'Na6', 'Nh6', 'Nf6',
+        'a6',  'a5',  'b6',  'b5',
+        'c6',  'c5',  'd6',  'd5',
+        'e6',  'e5',  'f6',  'f5',
+        'g6',  'g5',  'h6',  'h5'
+      ];
 
-    expect(moveSet).toEqual(openingMoveSet);
+    expect(moveSet).toEqual(whiteOpeningMoveSet);
+
+    chess.move('e4');
+    moveSet = AI.validMoves(chess.toFEN());
+
+    expect(moveSet).toEqual(blackOpeningMoveSet);
+
+    // late game position
+    let lateGameMoveSet = [
+        'Bh7', 'Bxf7', 'Ne7', 'Na7',
+        'Nxd6', 'Nb6', 'Kb5', 'Ka5',
+        'Kc4', 'Ka4', 'Kc3', 'Ka3', 
+        'Rxf3', 'Re3', 'Rd3', 'Rc3',
+        'Ra3', 'Rb2', 'Rb1', 'exd6', 
+        'd8=Q', 'd8=R', 'd8=B', 'd8=N',
+        'f6', 'e6', 'g4'
+    ];
+    //'d8=R', 'd8=B', 'd8=N' are not present in moveSet
+    
+    //'Kc5' is present in moveSet, but not in the lateGameMoveSet, which we know is correct.
+
+    chess = new Chess('2N3B1/2pP1p2/3p4/4PPr1/1K6/1R3pP1/3p1p1k/8 w - - 0 1');
+    moveSet = AI.validMoves(chess.toFEN());
+
+    console.log(moveSet);
+
+    expect(moveSet.length).toBe(lateGameMoveSet.length);
+    // expect(moveSet).toEqual()
 
 });
 
