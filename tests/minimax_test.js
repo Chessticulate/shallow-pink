e35a7ad8643df0d15881f1642b3924da4b980fa7
Bglaws <glaws.brian@gmail.com>
2023-03-27 18:14:31 -0400

    added memoization to minimax

--- tests/minimax_test.js
+++ tests/minimax_test.js
@@ -7,9 +7,10 @@ const Knight = require('../lib/pieces/knight');
 
 const AI = require('../lib/ai');
 const Chess = require('../lib/chess');
-const { nodeCount } = require('../lib/ai');
+const { nodeCount, memoizedNodes } = require('../lib/ai');
 
 let chess = new Chess();//('4b3/8/2BK1k1p/6pP/2P2p2/1p4b1/2p2qP1/4r2r w - - 0 1');
 
-console.log(AI.miniMax(chess.toFEN(), 4));
+console.log(AI.miniMax(chess.toFEN(), 3));
 console.log(AI.nodeCount);
+console.log(AI.memoizedNodes);
